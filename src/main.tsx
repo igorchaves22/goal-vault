57f5b420698c115c682816fe1eb5694c2405f003
Igor <contato.igorgabriel@hotmail.com>
2026-03-27 03:24:31 -0300

    🔨 refactor: update root element id

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { APP_ROOT_ID } from "~constants";
 import { App } from "./App";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(APP_ROOT_ID!).render(
     <StrictMode>
         <App />
     </StrictMode>
